da57fba5faf3f97c1ab8af4478d442a8c4e73a7d
aarush-gupta18 <2007aarush@gmail.com>
2026-06-07 16:54:08 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,11 +3,4 @@ export const fetchEvents = async () => {
     const response = await fetch(`${link}`);
     const data = await response.json();
     return data;};
-export const searchevents = async (a = {}) => {
-    const query = new URLSearchParams();
-    if (a.category)query.append('category', a.category);
-    if (a.day)query.append('day', a.day);
-    if (a.venue)query.append('venue', a.venue);
-    const response = await fetch(`${link}/search?${query.toString()}`);
-    const data = await response.json();
-    return data;};
+
